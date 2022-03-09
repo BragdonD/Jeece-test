89f372362c99e61587b696b3a3a036e34cf98913
Thomas DUCLOS <thomasduclos1012@gmail.com>
2022-03-09 17:07:28 +0100

    end swap logging form

--- app/src/login/Login.js
+++ app/src/login/Login.js
@@ -2,23 +2,111 @@ import { useEffect, useState } from "react";
 import { Form } from "./components/form.jsx";
 
 export const LoginForm = () => {
+    const [login, setLogin] = useState(false);
+    const [prenom, setPrenom] = useState("");
+    const [nom, setNom] = useState("");
+    const [email, setEmail] = useState("");
     const [pseudo, setPseudo] = useState("");
-
-    let login_data = [
-        {
-            placeholder: "Pseudo",
-            type: "text",
-            callback: setPseudo,
-        }
-    ];
+    const [password, setPassword] = useState("");
+    const [submit, setSubmit] = useState(false);
+    const [login_data, setLoginData] = useState([]);
 
     useEffect(() => {
-        console.log(pseudo);
-    })
+        setLoginData([
+            {
+                placeholder: "Pseudo",
+                type: "text",
+                callback: setPseudo,
+            },
+            {
+                placeholder: "Password",
+                type: "password",
+                callback: setPassword,
+            },
+            {
+                placeholder: "Envoyer",
+                type: "submit",
+                callback: setSubmit,
+            }
+        ]);
+    }, [])
+
+    const swapLogin = () => {
+        setLogin(!login);
+        if(!login) {
+            setLoginData([
+                {
+                    placeholder: "Prénom",
+                    type: "text",
+                    callback: setPrenom,
+                },
+                {
+                    placeholder: "Nom",
+                    type: "text",
+                    callback: setNom,
+                },
+                {
+                    placeholder: "Pseudo",
+                    type: "text",
+                    callback: setPseudo,
+                },
+                {
+                    placeholder: "Email",
+                    type: "email",
+                    callback: setEmail,
+                },
+                {
+                    placeholder: "Mot de Passe",
+                    type: "password",
+                    callback: setPassword,
+                },
+                {
+                    placeholder: "Envoyer",
+                    type: "submit",
+                    callback: setSubmit,
+                }
+            ]);
+        }
+        else {
+            setLoginData([
+                {
+                    placeholder: "Pseudo",
+                    type: "text",
+                    callback: setPseudo,
+                },
+                {
+                    placeholder: "Mot de Passe",
+                    type: "password",
+                    callback: setPassword,
+                },
+                {
+                    placeholder: "Envoyer",
+                    type: "submit",
+                    callback: setSubmit,
+                }
+            ]);
+        }
+    }
+
+    const renderLink = () => {
+        if(login) {
+            return(
+                <p>Pas encore membre <a href="#" onClick={swapLogin}>register</a>.</p>
+            )
+        }
+        else {
+            return(
+                <p>Déjà membre <a href="#" onClick={swapLogin}>login</a>.</p>
+            )
+        }
+    }
 
     return(
         <div>
             <Form inputs={login_data}></Form>
+            {
+                renderLink()
+            }
         </div>
     )
 };
--- app/src/login/components/inputs.jsx
+++ app/src/login/components/inputs.jsx
@@ -6,12 +6,19 @@ export const Input = ( { placeholder, type, callback } ) => {
      * @param {InputEvent} e 
      */
     const handleChange = (e) => {
+        console.log(e.target.value);
         callback(e.target.value);
     };
 
+    const handleClick = () => {
+        if(type === "submit") {
+            callback(true);
+        }
+    }
+
     return (
         <div>
-            <input type={type} placeholder={placeholder} onChange={handleChange}></input>
+            <input type={type} placeholder={placeholder} onChange={handleChange} onClick={handleClick}></input>
         </div>
     )
 }
